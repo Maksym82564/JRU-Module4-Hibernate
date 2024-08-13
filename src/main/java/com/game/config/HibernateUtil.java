cf66f52017c8a1b9487c8a7e503a5d4f304fdb61
Maksym Korniienko <maksym.korniienko25@gmail.com>
2024-08-13 18:41:57 +0300

    refactor: move session factory setup to HibernateUtil

---
+++ src/main/java/com/game/config/HibernateUtil.java
@@ -0,0 +1,35 @@
+package com.game.config;
+
+import com.game.entity.Player;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+
+import java.util.Properties;
+
+public class HibernateUtil {
+    private static HibernateUtil instance;
+    private final SessionFactory sessionFactory;
+
+    private HibernateUtil() {
+        Properties properties = new Properties();
+        properties.put(Environment.DRIVER, "com.p6spy.engine.spy.P6SpyDriver");
+        properties.put(Environment.URL, "jdbc:p6spy:mysql://localhost:3306/rpg");
+        properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
+        properties.put(Environment.USER, "root");
+        properties.put(Environment.PASS, "admin");
+        properties.put(Environment.HBM2DDL_AUTO, "update");
+
+        sessionFactory = new Configuration()
+                .setProperties(properties)
+                .addAnnotatedClass(Player.class)
+                .buildSessionFactory();
+    }
+
+    public static SessionFactory getSessionFactory() {
+        if (instance == null) {
+            instance = new HibernateUtil();
+        }
+        return instance.sessionFactory;
+    }
+}
--- src/main/java/com/game/repository/PlayerRepositoryDB.java
+++ src/main/java/com/game/repository/PlayerRepositoryDB.java
@@ -1,18 +1,16 @@
 package com.game.repository;
 
+import com.game.config.HibernateUtil;
 import com.game.entity.Player;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.cfg.Environment;
 import org.hibernate.query.Query;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.PreDestroy;
 import java.util.List;
 import java.util.Optional;
-import java.util.Properties;
 
 @Repository(value = "db")
 public class PlayerRepositoryDB implements IPlayerRepository {
@@ -20,17 +18,7 @@ public class PlayerRepositoryDB implements IPlayerRepository {
     private final SessionFactory sessionFactory;
 
     public PlayerRepositoryDB() {
-        Properties properties = new Properties();
-        properties.put(Environment.DRIVER, "com.p6spy.engine.spy.P6SpyDriver");
-        properties.put(Environment.URL, "jdbc:p6spy:mysql://localhost:3306/rpg");
-        properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
-        properties.put(Environment.USER, "root");
-        properties.put(Environment.PASS, "admin");
-        properties.put(Environment.HBM2DDL_AUTO, "update");
-        sessionFactory = new Configuration()
-                .setProperties(properties)
-                .addAnnotatedClass(Player.class)
-                .buildSessionFactory();
+        sessionFactory = HibernateUtil.getSessionFactory();
     }
 
     @Override
